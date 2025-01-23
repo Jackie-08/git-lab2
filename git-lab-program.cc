2cee4ff281432f23bac2c6156fa4fac25fe832f5
Jackie <jh846023@ohio.edu>
2025-01-22 20:23:35 -0500

    program complete

---
+++ git-lab-program.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib> 
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
